0c2111382a58e75b7470682ca3e7376c84faf1ae
Oscar Ignacio Osses Arriaza <oscarossesa@gmail.com>
2016-06-08 21:04:16 -0300

    Limpio código, solo funcionalidad pintar contactos y contador

--- NotificacionesEnTiempoReal/NotificationComponent.cs
+++ NotificacionesEnTiempoReal/NotificationComponent.cs
@@ -18,12 +18,15 @@ namespace NotificacionesEnTiempoReal
 
             string conString = ((NameValueCollection)ConfigurationManager.GetSection("ConexionesBD"))["DemoSignalr"];
 
-            string sqlCommand = @"SELECT [ContactID], [ContactName], [ContactNo] from [dbo].[Contacts] where [AddedOn] > @AddedOn";
+            //string sqlCommand = @"SELECT [ContactID], [ContactName], [ContactNo] from [dbo].[Contacts] where [AddedOn] > @AddedOn";
+
+            string sqlCommand = @"SELECT [ContactID], [ContactName], [ContactNo] from [dbo].[Contacts]";
 
             using (SqlConnection con = new SqlConnection(conString))
             {
                 SqlCommand cmd = new SqlCommand(sqlCommand, con);
-                cmd.Parameters.AddWithValue("@AddedOn", currentTime);
+                //cmd.Parameters.AddWithValue("@AddedOn", currentTime);
+
 
                 if (con.State != System.Data.ConnectionState.Open)
                 {
@@ -34,7 +37,7 @@ namespace NotificacionesEnTiempoReal
                 SqlDependency sqlDep = new SqlDependency(cmd);
                 sqlDep.OnChange += sqlDependency_OnChange;
 
-                using(SqlDataReader reader = cmd.ExecuteReader())
+                using (SqlDataReader reader = cmd.ExecuteReader())
                 {
 
                 }
@@ -44,32 +47,42 @@ namespace NotificacionesEnTiempoReal
         private void sqlDependency_OnChange(object sender, SqlNotificationEventArgs e)
         {
             //or you can also check => if (e.Info == SqlNotificationInfo.Insert) , if you want notification only for inserted record
-            if (e.Type == SqlNotificationType.Change)
+            //if (e.Type == SqlNotificationType.Change)
+            //{
+            //    SqlDependency sqlDep = sender as SqlDependency;
+            //    sqlDep.OnChange -= sqlDependency_OnChange;
+
+            //    //from here we will send notification message to client
+            //    var notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+            //    notificationHub.Clients.All.notify("added");
+            //}
+
+            if (e.Info == SqlNotificationInfo.Insert)
             {
                 SqlDependency sqlDep = sender as SqlDependency;
                 sqlDep.OnChange -= sqlDependency_OnChange;
 
                 //from here we will send notification message to client
                 var notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
-                notificationHub.Clients.All.notify("added");
+                notificationHub.Clients.All.notify("nuevoContacto");
             }
 
-            if (e.Info == SqlNotificationInfo.Insert)
-            {
-                //This is how signalrHub can be accessed outside the SignalR Hub Notification.cs file
-                var notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+            //if (e.Info == SqlNotificationInfo.Insert)
+            //{
+            //    //This is how signalrHub can be accessed outside the SignalR Hub Notification.cs file
+            //    var notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
 
-                NotificationComponent NC = new NotificationComponent();
-                var list = NC.ObtenerContactos(DateTime.Now); //Dapper
+            //    NotificationComponent NC = new NotificationComponent();
+            //    var list = NC.ObtenerContactos(DateTime.Now); //Dapper
 
-                //LastRun = DateTime.Now.ToUniversalTime();
+            //    //LastRun = DateTime.Now.ToUniversalTime();
 
-                foreach (var item in list)
-                {
-                    //replace domain name with your own domain name
-                    notificationHub.Clients.User("<DomainName>" + item.ContactID).addLatestNotification(item);
-                }
-            }
+            //    foreach (var item in list)
+            //    {
+            //        //replace domain name with your own domain name
+            //        notificationHub.Clients.User("<DomainName>" + item.ContactID).addLatestNotification(item);
+            //    }
+            //}
 
             //Call the RegisterNotification method again
             RegisterNotification(DateTime.Now);
--- NotificacionesEnTiempoReal/Scripts/chilecompra.notificador.js
+++ NotificacionesEnTiempoReal/Scripts/chilecompra.notificador.js
@@ -1,106 +1,164 @@
 ﻿jQuery.Notificador = function (options) {
 
-	var defaults = {
-		Controladores:
+    var defaults = {
+        Controladores:
         {
-        	GetPrecioCaro: 'PrecioCaro/Index',
-        	GetCantidadReportes: 'PrecioCaro/CantidadReportes'
+            GetPrecioCaro: 'PrecioCaro/Index',
+            GetCantidadReportes: 'PrecioCaro/CantidadReportes'
         },
-		Tipo: 0
-	};
+        Tipo: 0
+    };
 
-	var config = $.extend(defaults, options);
+    var config = $.extend(defaults, options);
 
-	jQuery.Notificador.Init = function Init() {
+    jQuery.Notificador.Init = function Init() {
 
-	    updateContact();
+        verContactos();
+        actualizarContador();
 
-		// Click on notification icon for show notification
-		$('span.noti').click(function (e) {
-			e.stopPropagation();
-			$('.noti-content').show();
-			var count = 0;
-			count = parseInt($('span.count').html()) || 0;
-		    //only load notification if not already loaded
-			updateNotification();
-			//if (count > 0) {
-			//	updateNotification();
-			//}
-			$('span.count', this).html('&nbsp;');
-		})
+        // ver contactos
+        function verContactos() {
+            $.ajax({
+                type: 'GET',
+                //url: '/Home/GetNotificationContacts',
+                //url: '@Url.Action("GetNotificationContacts", "Home")',
+                //url: 'Home/GetNotificationContacts',
+                url: 'Notificador/Home/GetNotificationContacts',
+                success: function (response) {
+                    $('#contentContacts').empty();
+                    $.each(response, function (index, value) {
+                        $('#contentContacts').append($('<li>' + value.ContactName + '</li>'));
+                    });
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        }
 
-		// hide notifications
-		$('html').click(function () {
-			$('.noti-content').hide();
-		})
+        // update notification count
+        function actualizarContador() {
+            var count = 0;
 
-		// update notification
-		function updateNotification() {
-			$('#notiContent').empty();
-			$('#notiContent').append($('<li>Loading...</li>'));
-			$.ajax({
-			    type: 'GET',
-			    //url: '/home/GetNotificationContacts',
-			    //url: '@Url.Action("GetNotificationContacts", "Home")',
-			    url: 'Home/GetNotificationContacts',
-			    success: function (response) {
-			        $('#notiContent').empty();
-			        if (response.length == 0) {
-			            $('#notiContent').append($('<li>No data available</li>'));
-			        }
-			        $.each(response, function (index, value) {
-			            $('#notiContent').append($('<li>New contact : ' + value.ContactName + ' (' + value.ContactNo + ') added</li>'));
-			        });
-			    },
-			    error: function (error) {
-			        console.log(error);
-			    }
-			});
-		}
+            $.ajax({
+                type: 'GET',
+                //url: '/Home/GetNotificationContacts',
+                //url: '@Url.Action("GetNotificationContacts", "Home")',
+                //url: 'Home/GetNotificationContacts',
+                url: 'Notificador/Home/GetNotificationContacts',
+                success: function (response) {
+                    count = parseInt(response.length);
+                    $('span.count').html(count);
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        }
 
-		// update notification count
-		function updateNotificationCount() {
-			var count = 0;
-			count = parseInt($('span.count').html()) || 0;
-			count++;
-			$('span.count').html(count);
-		}
+        // signalr js code for start hub and send receive notification
+        var notificationHub = $.connection.notificationHub;
 
-	    // update notification count
-		function updateContact() {
-		    $.ajax({
-		        type: 'GET',
-		        url: 'Home/GetNotificationContacts',
-		        success: function (response) {
-		            $.each(response, function (index, value) {
-		                $('#contentContacts').append($('<li>New contact : ' + value.ContactName + ' (' + value.ContactNo + ') added</li>'));
-		            });
-		        },
-		        error: function (error) {
-		            console.log(error);
-		        }
-		    });
-		}
+        $.connection.hub.start().done();
 
-		// signalr js code for start hub and send receive notification
-		var notificationHub = $.connection.notificationHub;
-		$.connection.hub.start().done(function () {
-			console.log('Notification hub started');
-		});
+        //signalr method for push server message to client
+        notificationHub.client.notify = function (message) {
+            if (message && message.toLowerCase() == "nuevocontacto") {
+                verContactos();
+                actualizarContador();
+            }
+        }
 
-		//signalr method for push server message to client
-		notificationHub.client.notify = function (message) {
-			if (message && message.toLowerCase() == "added") {
-			    updateNotificationCount();
-			    updateContact();
-			}
-		}
+    };
 
-	};
+    //jQuery.Notificador.Init = function Init() {
 
-	//jQuery.PrecioCaro.MostrarContador = function MostrarContador() {
+    //    updateContact();
 
-		
-	//};
+    //    // Click on notification icon for show notification
+    //    $('span.noti').click(function (e) {
+    //        e.stopPropagation();
+    //        $('.noti-content').show();
+    //        var count = 0;
+    //        count = parseInt($('span.count').html()) || 0;
+    //        //only load notification if not already loaded
+    //        updateNotification();
+    //        //if (count > 0) {
+    //        //	updateNotification();
+    //        //}
+    //        $('span.count', this).html('&nbsp;');
+    //    })
 
+    //    // hide notifications
+    //    $('html').click(function () {
+    //        $('.noti-content').hide();
+    //    })
+
+    //    // update notification
+    //    function updateNotification() {
+    //        $('#notiContent').empty();
+    //        $('#notiContent').append($('<li>Loading...</li>'));
+    //        $.ajax({
+    //            type: 'GET',
+    //            //url: '/home/GetNotificationContacts',
+    //            //url: '@Url.Action("GetNotificationContacts", "Home")',
+    //            //url: 'Home/GetNotificationContacts',
+    //            url: 'Notificador/Home/GetNotificationContacts',
+    //            success: function (response) {
+    //                $('#notiContent').empty();
+    //                if (response.length == 0) {
+    //                    $('#notiContent').append($('<li>No data available</li>'));
+    //                }
+    //                $.each(response, function (index, value) {
+    //                    $('#notiContent').append($('<li>New contact : ' + value.ContactName + ' (' + value.ContactNo + ') added</li>'));
+    //                });
+    //            },
+    //            error: function (error) {
+    //                console.log(error);
+    //            }
+    //        });
+    //    }
+
+    //    // update notification count
+    //    function updateNotificationCount() {
+    //        var count = 0;
+    //        count = parseInt($('span.count').html()) || 0;
+    //        count++;
+    //        $('span.count').html(count);
+    //    }
+
+    //    // update notification count
+    //    function updateContact() {
+    //        $.ajax({
+    //            type: 'GET',
+    //            //url: '/Home/GetNotificationContacts',
+    //            //url: '@Url.Action("GetNotificationContacts", "Home")',
+    //            //url: 'Home/GetNotificationContacts',
+    //            url: 'Notificador/Home/GetNotificationContacts',
+    //            success: function (response) {
+    //                $.each(response, function (index, value) {
+    //                    $('#contentContacts').append($('<li>New contact : ' + value.ContactName + ' (' + value.ContactNo + ') added</li>'));
+    //                });
+    //            },
+    //            error: function (error) {
+    //                console.log(error);
+    //            }
+    //        });
+    //    }
+
+    //    // signalr js code for start hub and send receive notification
+    //    var notificationHub = $.connection.notificationHub;
+    //    $.connection.hub.start().done(function () {
+    //        console.log('Notification hub started');
+    //    });
+
+    //    //signalr method for push server message to client
+    //    notificationHub.client.notify = function (message) {
+    //        if (message && message.toLowerCase() == "added") {
+    //            updateNotificationCount();
+    //            updateContact();
+    //        }
+    //    }
+
+    //};
 }
