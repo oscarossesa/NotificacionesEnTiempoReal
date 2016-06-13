47615b3b0adf54d873de5ff70bacce5bf11723a7
Oscar Ignacio Osses Arriaza <oscarossesa@gmail.com>
2016-06-13 15:41:07 -0300

    Agrego notify.min.js

--- NotificacionesEnTiempoReal/Scripts/chilecompra.notificador.js
+++ NotificacionesEnTiempoReal/Scripts/chilecompra.notificador.js
@@ -1,19 +1,19 @@
-﻿jQuery.Notificador = function (options) {
+﻿$.Notificador = function (options) {
 
     var defaults = {
         Controladores:
         {
-            A: '/Home/GetNotificationContacts',
-            B: '@Url.Action("GetNotificationContacts", "Home")',
-            C: 'Notificador/Home/GetNotificationContacts',
-            D: 'Home/GetNotificationContacts'
+            //GetNotificationContacts: '/Home/GetNotificationContacts'
+            //GetNotificationContacts: '@Url.Action("GetNotificationContacts", "Home")'
+            GetNotificationContacts: 'Notificador/Home/GetNotificationContacts'
+            //GetNotificationContacts: 'Home/GetNotificationContacts'
         },
         Tipo: 0
     };
 
     var config = $.extend(defaults, options);
 
-    jQuery.Notificador.Init = function Init() {
+    $.Notificador.Init = function Init() {
 
         // signalr js code for start hub and send receive notification
         var notificationHub = $.connection.notificationHub;
@@ -23,16 +23,17 @@
         //signalr method for push server message to client
         notificationHub.client.notify = function (message) {
             if (message && message.toLowerCase() == "nuevocontacto") {
-                jQuery.Notificador.ActualizarContadorNotificacion();
-                jQuery.Notificador.ListarContactos();
+                $.Notificador.ActualizarContadorNotificacion();
+                $.Notificador.ListarContactos();
+                $.Notificador.LicitacionPublicada();
             }
-        }        
+        }
 
-        jQuery.Notificador.EsconderNotificacion();
+        $.Notificador.EsconderNotificacion();
 
     };
 
-    jQuery.Notificador.ActualizarContadorNotificacion = function ActualizarContadorNotificacion() {
+    $.Notificador.ActualizarContadorNotificacion = function ActualizarContadorNotificacion() {
 
         var count = 0;
         count = parseInt(jQuery.Notificador.CantidadUsuarios());
@@ -40,14 +41,16 @@
 
     };
 
-    jQuery.Notificador.CantidadUsuarios = function CantidadUsuarios() {
+    $.Notificador.CantidadUsuarios = function CantidadUsuarios() {
+
+        var count = 0;
 
         $.ajax({
             type: 'GET',
-            url: config.Controladores.D,
+            url: config.Controladores.GetNotificationContacts,
             async: false,
             success: function (response) {
-                count = parseInt(response.length);                
+                count = parseInt(response.length);
             },
             error: function (error) {
                 console.log(error);
@@ -56,12 +59,12 @@
 
         return count;
     };
-    
-    jQuery.Notificador.ListarContactos = function ListarContactos() {
+
+    $.Notificador.ListarContactos = function ListarContactos() {
 
         $.ajax({
             type: 'GET',
-            url: config.Controladores.D,
+            url: config.Controladores.GetNotificationContacts,
             success: function (response) {
                 $('#contentContacts').empty();
                 $.each(response, function (index, value) {
@@ -73,15 +76,15 @@
             }
         });
 
-    };    
+    };
 
-    jQuery.Notificador.VerContenidoNotificacion = function VerContenidoNotificacion() {
+    $.Notificador.VerContenidoNotificacion = function VerContenidoNotificacion() {
 
         $('#notiContent').empty();
-        $('#notiContent').append($('<li>Loading...</li>'));
+        $('#notiContent').append($('<li>Cargando...</li>'));
         $.ajax({
             type: 'GET',
-            url: config.Controladores.D,
+            url: config.Controladores.GetNotificationContacts,
             success: function (response) {
                 $('#notiContent').empty();
                 if (response.length == 0) {
@@ -96,117 +99,21 @@
             }
         });
 
-    };   
+    };
 
-    jQuery.Notificador.EsconderNotificacion = function EsconderNotificacion() {
-    
+    $.Notificador.EsconderNotificacion = function EsconderNotificacion() {
+
         $('html').click(function () {
             $('.noti-content').hide();
         })
-    
-    };
-    
-/*
-
-    jQuery.Notificador.VerContactos = function VerContactos() {
-
-    
 
     };
 
-*/
+    $.Notificador.LicitacionPublicada = function LicitacionPublicada() {
 
+        $.notify("Se ha publicado una nueva licitación: Data Center y Servicios Asociados.", "success");
 
+    };
 
-
-    //jQuery.Notificador.Init = function Init() {
-
-    //    updateContact();
-
-    //    // Click on notification icon for show notification
-    //    $('span.noti').click(function (e) {
-    //        e.stopPropagation();
-    //        $('.noti-content').show();
-    //        var count = 0;
-    //        count = parseInt($('span.count').html()) || 0;
-    //        //only load notification if not already loaded
-    //        updateNotification();
-    //        //if (count > 0) {
-    //        //	updateNotification();
-    //        //}
-    //        $('span.count', this).html('&nbsp;');
-    //    })
-
-    //    // hide notifications
-    //    $('html').click(function () {
-    //        $('.noti-content').hide();
-    //    })
-
-    //    // update notification
-    //    function updateNotification() {
-    //        $('#notiContent').empty();
-    //        $('#notiContent').append($('<li>Loading...</li>'));
-    //        $.ajax({
-    //            type: 'GET',
-    //            //url: '/home/GetNotificationContacts',
-    //            //url: '@Url.Action("GetNotificationContacts", "Home")',
-    //            //url: 'Home/GetNotificationContacts',
-    //            url: 'Notificador/Home/GetNotificationContacts',
-    //            success: function (response) {
-    //                $('#notiContent').empty();
-    //                if (response.length == 0) {
-    //                    $('#notiContent').append($('<li>No data available</li>'));
-    //                }
-    //                $.each(response, function (index, value) {
-    //                    $('#notiContent').append($('<li>New contact : ' + value.ContactName + ' (' + value.ContactNo + ') added</li>'));
-    //                });
-    //            },
-    //            error: function (error) {
-    //                console.log(error);
-    //            }
-    //        });
-    //    }
-
-    //    // update notification count
-    //    function updateNotificationCount() {
-    //        var count = 0;
-    //        count = parseInt($('span.count').html()) || 0;
-    //        count++;
-    //        $('span.count').html(count);
-    //    }
-
-    //    // update notification count
-    //    function updateContact() {
-    //        $.ajax({
-    //            type: 'GET',
-    //            //url: '/Home/GetNotificationContacts',
-    //            //url: '@Url.Action("GetNotificationContacts", "Home")',
-    //            //url: 'Home/GetNotificationContacts',
-    //            url: 'Notificador/Home/GetNotificationContacts',
-    //            success: function (response) {
-    //                $.each(response, function (index, value) {
-    //                    $('#contentContacts').append($('<li>New contact : ' + value.ContactName + ' (' + value.ContactNo + ') added</li>'));
-    //                });
-    //            },
-    //            error: function (error) {
-    //                console.log(error);
-    //            }
-    //        });
-    //    }
-
-    //    // signalr js code for start hub and send receive notification
-    //    var notificationHub = $.connection.notificationHub;
-    //    $.connection.hub.start().done(function () {
-    //        console.log('Notification hub started');
-    //    });
-
-    //    //signalr method for push server message to client
-    //    notificationHub.client.notify = function (message) {
-    //        if (message && message.toLowerCase() == "added") {
-    //            updateNotificationCount();
-    //            updateContact();
-    //        }
-    //    }
-
-    //};
+    
 }
